c4cb2383319a09e66aff0f8433ea092200020dbb
slopezb16 <79761738+slopezb16@users.noreply.github.com>
2025-11-10 18:59:29 -0500

    feat: Sección 24 Update 2023 ASP.NET Parte 1

---
+++ Sección 24 Update 2023 ASP.NET Parte 1/Ejercicio1ASP/Ejercicio1ASP/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Ejercicio1ASP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Exportar</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2 style="color:red">Boton exporta excel</h2>
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Cargar archivo" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Sección 24 Update 2023 ASP.NET Parte 1/Ejercicio1ASP/Ejercicio1ASP/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Data;
+using System.IO;
+
+namespace Ejercicio1ASP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable dt = new DataTable("BookExcel");
+
+                dt.Columns.Add("Nombre");
+                dt.Columns.Add("Apellido");
+                dt.Columns.Add("Telefono");
+                dt.Columns.Add("Edad");
+
+                DataRow dr = dt.NewRow();
+                dr["Nombre"] = "Jonathan";
+                dr["Apellido"] = "Robayo";
+                dr["Telefono"] = "123456";
+                dr["Edad"] = "30";
+
+                dt.Rows.Add(dr);
+
+                using (XLWorkbook book = new XLWorkbook())
+                {
+                    var hoja = book.Worksheets.Add(dt);
+                    hoja.ColumnsUsed().AdjustToContents();
+                    Response.Clear();
+                    Response.Buffer = true;
+                    Response.Charset = "";
+                    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    Response.AddHeader("content-disposition", "attachment;filename=Book.xlsx");
+
+                    using (MemoryStream memoria = new MemoryStream())
+                    {
+                        book.SaveAs(memoria);
+                        Response.BinaryWrite(memoria.ToArray());
+                        Response.Flush();
+                        Response.End();
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
